9e64047386cc1dc9f7bf8571f1fbdc2d7857182f
Nils Munch <nils@lizardfactory.com>
2017-05-04 11:10:56 +0200

    Basic functionality

---
+++ Vendor/PlayPylonClient.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright 2017 PlayPylon LtD.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ * use, copy, modify, and distribute this software in source code or binary
+ * form for use in connection with the web services and APIs provided by
+ * PlayPylon.
+ *
+ */
+namespace PlayPylon;
+
+class PlayPylonClient
+{
+
+}
